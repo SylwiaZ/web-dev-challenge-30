ea3f313d4d5b26c2b65559c09456b840ffca16f7
Sylwia Zuziak <sylwia.zuz@gmail.com>
2017-03-16 18:29:25 +0100

    Finished desktop structure of project

---
+++ public/asset/js/script.js
@@ -0,0 +1,12 @@
+function initMap() {
+    'use strict';
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementsByClassName('contact-map')[0], {
+        center: {lat: 50.055389, lng: 19.935309},
+        scrollwheel: true,
+        zoom: 12,
+        mapTypeControl: false,
+        streetViewControl: false,
+        zoomControl: false
+    });
+}
