88b65806535efe7c16ae8111470640d355305d46
Sylwia Zuziak <sylwia.zuz@gmail.com>
2017-06-12 12:49:12 +0200

    Refactoring

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,27 +7,52 @@ module.exports = function (grunt) {
     require('jit-grunt')(grunt);
 
     /**
+     * Load combine media queries
+     */
+    grunt.loadNpmTasks('grunt-combine-media-queries');
+
+    /**
      * Project config
      */
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-		sass: {
-			dist: {
-				files: {
-					'public/asset/css/style.css': 'src/scss/style.scss'
-				}
-			}
+        cmq: {
+            dist: {
+                files: {
+                    'public/asset/css/style.css': 'public/asset/css/style.css'
+                }
+            }
         },
-		watch: {
-			sass: {
-				files: ['**/*.scss'],
-				tasks: ['sass'],
-				options: {
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({browsers: 'last 2 versions'})
+                ]
+            },
+            dist: {
+                src: 'public/asset/css/style.css'
+            }
+        },
+        sass: {
+            options: {
+                outputStyle: 'expanded'
+            },
+            dist: {
+                files: {
+                    'public/asset/css/style.css': 'src/scss/main.scss'
+                }
+            }
+        },
+        watch: {
+            sass: {
+                files: ['**/*.scss'],
+                tasks: ['sass', 'postcss', 'cmq'],
+                options: {
                     livereload: true,
                     spawn: false
                 }
-			}
+            }
         }
     });
 
--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -11,12 +11,45 @@ function initMap() {
     });
 }
 
-$(document).ready(function(){
-    
+$(document).ready(function() {
+    handleWindowScroll();
+    handleNavigation();
+});
+
+function handleWindowScroll() {
+    $(window).scroll(function() {
+        /*Przyklejenie nawigacji*/
+        var top = $(window).scrollTop();
+        var find_class_small = $.contains('.nav-bar', '.small');
+
+        if(top > 200 && find_class_small == false) {
+            $('.nav-bar').addClass('small');
+        }
+        else {
+            $('.nav-bar').removeClass('small');
+        }
+    });
+}
+
+function handleNavigation() {
+    /*Rozwijanie mobilnej nawigacji*/
     $('.nav-bar-rwd .hamburger').click(function() {
         $(this).parents('.nav-bar-rwd').toggleClass('open');
-    })
-    
-}); 
-                  
-                  
+    });
+
+    /*Przewijanie do sekcji*/
+    $('.nav-bar .link, .nav-bar-rwd .link, .headline-1 .link').click(function(event) {
+        event.preventDefault();
+        var anchor = $(this).attr('href');
+        console.log(anchor);
+        if($(anchor).length > 0) {
+            var distanceFromTop = $(anchor).offset().top;
+            $('.nav-bar-rwd').removeClass('open');
+            $('html, body').animate({
+                scrollTop: distanceFromTop
+            }, 500, function() {
+                location.hash = anchor;
+            });
+        }
+    });
+}
