fe7b250d4116b4272634efae15818d7c59b3a749
Sylwia Zuziak <sylwia.zuz@gmail.com>
2017-06-12 18:48:02 +0200

    Favicons, mobile nav fixes

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,16 +1,3 @@
-function initMap() {
-    'use strict';
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementsByClassName('contact-map')[0], {
-        center: {lat: 50.055389, lng: 19.935309},
-        scrollwheel: true,
-        zoom: 12,
-        mapTypeControl: false,
-        streetViewControl: false,
-        zoomControl: false
-    });
-}
-
 $(document).ready(function() {
     handleWindowScroll();
     handleNavigation();
@@ -34,22 +21,37 @@ function handleWindowScroll() {
 function handleNavigation() {
     /*Rozwijanie mobilnej nawigacji*/
     $('.nav-bar-rwd .hamburger').click(function() {
-        $(this).parents('.nav-bar-rwd').toggleClass('open');
+        $('body').toggleClass('open');
     });
 
     /*Przewijanie do sekcji*/
     $('.nav-bar .link, .nav-bar-rwd .link, .headline-1 .link').click(function(event) {
         event.preventDefault();
         var anchor = $(this).attr('href');
-        console.log(anchor);
         if($(anchor).length > 0) {
             var distanceFromTop = $(anchor).offset().top;
-            $('.nav-bar-rwd').removeClass('open');
+            $('body').removeClass('open');
             $('html, body').animate({
                 scrollTop: distanceFromTop
             }, 500, function() {
                 location.hash = anchor;
             });
         }
+    });
+}
+
+function initMap() {
+    'use strict';
+    // Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementsByClassName('contact-map')[0], {
+        center: {lat: 50.055389, lng: 19.935309},
+        scrollwheel: false,
+        zoom: 12,
+        mapTypeControl: false,
+        streetViewControl: false
+    });
+
+    google.maps.event.addListener(map, 'click', function(event){
+        this.setOptions({scrollwheel: true});
     });
 }
